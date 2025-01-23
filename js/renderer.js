8cbd732a7b1263ce77c076ce55fda06c4a324366
amoynahan89 <amoynahan89@gmail.com>
2025-01-22 21:06:14 -0800

    cleanup

--- js/renderer.js
+++ js/renderer.js
@@ -33,22 +33,3 @@ export class Renderer {
         this.ctx.putImageData(imageData, 0, 0); // Render the updated imageData
     }
 }
-
-// // Set RGBA values
-// data[pixelIndex] = particle.color.r;     // Red
-// data[pixelIndex + 1] = particle.color.g; // Green
-// data[pixelIndex + 2] = particle.color.b; // Blue
-// data[pixelIndex + 3] = particle.color.a; // Alpha (255 for opaque)
-
-
-// this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-
-// for (const particle of grid.activeParticles) {
-//     this.ctx.fillStyle = particle.color;
-//     this.ctx.fillRect(
-//         particle.x * particle.size,
-//         particle.y * particle.size,
-//         particle.size,
-//         particle.size
-//     );
-// }
