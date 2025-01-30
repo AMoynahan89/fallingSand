f54b12e772a42d91a6d08a1f16b564e49477d179
amoynahan89 <amoynahan89@gmail.com>
2025-01-29 23:42:42 -0800

    image data

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const HEIGHT = 600;
+const WIDTH = 800;
+
+const canvas = document.getElementById('canvas');   
+const ctx = canvas.getContext('2d');
+const imgData = ctx.createImageData(WIDTH, HEIGHT);
+
+for (let i = 0; i < imgData.data.length; i += 4) {
+    imgData.data[i + 0] = 255; // R value
+    imgData.data[i + 1] = 0; // G value
+    imgData.data[i + 2] = 0; // B value
+    imgData.data[i + 3] = 255; // A value
+}
+
+ctx.putImageData(imgData, 0, 0);
+
+for (let i = 900000; i < imgData.data.length; i += 4) {
+    imgData.data[i + 0] = 0; // R value
+    imgData.data[i + 1] = 255; // G value
+    imgData.data[i + 2] = 0; // B value
+    imgData.data[i + 3] = 255; // A value
+}
+
+ctx.putImageData(imgData, 0, 0);
